cb063fc7aeb9322895a87eb9b0d5843404801588
Simon <52527080+twentysixreds@users.noreply.github.com>
2026-01-05 21:25:33 +0000

    feat: add ThemeSwitcher component and integrate theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { ErrorBoundary } from "react-error-boundary";
+import { ThemeProvider } from 'next-themes'
 
 import App from './App.tsx'
 import { ErrorFallback } from './ErrorFallback.tsx'
@@ -8,6 +9,14 @@ import "./main.css"
 
 createRoot(document.getElementById('root')!).render(
   <ErrorBoundary FallbackComponent={ErrorFallback}>
-    <App />
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="zinc"
+      themes={['zinc', 'nature', 'rose']}
+      enableColorScheme={false}
+      storageKey="baseline-theme"
+    >
+      <App />
+    </ThemeProvider>
    </ErrorBoundary>
 )
